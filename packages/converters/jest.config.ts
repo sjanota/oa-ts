a581d46067d44fe444f0fa47a36804c14a1b8998
Szymon Janota <szymon.janota@pubnub.com>
2023-04-06 20:36:03 +0200

    Rename io-ts-openapi to converters

--- packages/converters/jest.config.ts
+++ packages/converters/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'io-ts-openapi',
+  displayName: 'converters',
   preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/io-ts-openapi',
+  coverageDirectory: '../../coverage/packages/converters',
   testTimeout: 1000,
 };
