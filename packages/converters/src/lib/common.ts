776fef3bbfdfe6e0321b77c8a6e2d6f02863795e
Szymon Janota <szymon.janota@pubnub.com>
2023-04-06 21:23:41 +0200

    reexport commonly used stuff

--- packages/converters/src/lib/common.ts
+++ packages/converters/src/lib/common.ts
@@ -1,5 +1,8 @@
 import { isNumber } from 'fp-ts/lib/number';
 import { OpenAPIV3_1 as openapi } from 'openapi-types';
+import * as io from 'io-ts';
+
+export { io, openapi };
 
 export const minimum = (schema: openapi.SchemaObject) =>
   isNumber(schema.exclusiveMinimum) ? schema.exclusiveMinimum : schema.minimum;
