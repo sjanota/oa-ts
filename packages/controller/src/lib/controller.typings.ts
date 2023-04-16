407463f7752526b50cb14017e1bc592b1ae52a3a
Szymon Janota <szymon.janota@pubnub.com>
2023-04-16 11:51:30 +0200

    Do nto use exmape schema in router tests

--- packages/controller/src/lib/controller.typings.ts
+++ packages/controller/src/lib/controller.typings.ts
@@ -6,7 +6,6 @@ import {
   FlattenedPaths,
   PathsWithPrefixedMethods,
 } from './controller';
-import { doc, Error, Id, User } from './example-schema';
 
 type TestPaths = {
   '/users': {
@@ -78,7 +77,108 @@ type _FlattenedPathsTest = Expect<
   >
 >;
 
+const doc = {
+  openapi: '3.1.0',
+  info: {
+    title: 'test',
+    version: '1.0.0',
+  },
+  paths: {
+    '/users/:id': {
+      get: {
+        operationId: 'getUserById',
+        parameters: [
+          {
+            $ref: '#/components/parameters/PathId',
+          },
+        ],
+        responses: {
+          200: {
+            $ref: '#/components/responses/UserByIdOK',
+          },
+          404: {
+            summary: 'User not found',
+            $ref: '#/components/responses/Error',
+          },
+        },
+      },
+    },
+    '/users': {
+      post: {
+        operationId: 'createUser',
+        parameters: [
+          {
+            name: 'data',
+            in: 'body',
+            schema: { $ref: '#/components/schemas/User' },
+          },
+        ],
+        responses: {
+          200: {
+            $ref: '#/components/responses/UserByIdOK',
+          },
+          404: {
+            summary: 'User not found',
+            $ref: '#/components/responses/Error',
+          },
+        },
+      },
+    },
+  },
+  components: {
+    responses: {
+      UserByIdOK: {
+        description: 'aaaa',
+        content: {
+          'application/json': {
+            schema: { $ref: '#/components/schemas/User' },
+          },
+        },
+      },
+      Error: {
+        description: 'Error message',
+        content: {
+          'application/json': {
+            schema: { $ref: '#/components/schemas/Error' },
+          },
+        },
+      },
+    },
+    parameters: {
+      PathId: {
+        name: 'id',
+        in: 'path',
+        schema: { $ref: '#/components/schemas/Id' },
+      },
+    },
+    schemas: {
+      User: {
+        type: 'object',
+        properties: {
+          name: { type: 'string' },
+          id: { type: 'number' },
+        },
+      },
+      Id: {
+        type: 'number',
+      },
+      Error: {
+        type: 'object',
+        properties: {
+          error: {
+            type: 'string',
+          },
+        },
+      },
+    },
+  },
+} as const;
+
 declare const controller: Controller<typeof doc>;
+
+type Id = number;
+type User = { readonly name?: string; readonly id?: number };
+type Error = { readonly error?: string };
 
 type _Test = Expect<
   Equal<
